051bcc937835e47a0315cc06b7887b8b5741792a
jcbrooks <lilman_2010@hotmail.com>
2019-07-19 23:26:20 -0400

    Main Comiit

--- ServiceBusWebJob/Program.cs
+++ ServiceBusWebJob/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
+using Microsoft.Extensions.Logging.Console;
 
 namespace ServiceBusWebJob
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+            {
+                b.AddAzureStorageCoreServices();
+                b.AddServiceBus(sbOptions =>
+                {
+                    sbOptions.MessageHandlerOptions.AutoComplete = true;
+                    sbOptions.MessageHandlerOptions.MaxConcurrentCalls = 16;
+                });
+                                                
+            });
+            builder.ConfigureLogging((context, b) =>
+            {
+                b.AddConsole();
+            });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
         }
     }
 }
---
+++ ServiceBusWebJob/ServiceBus.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceBusWebJob
+{
+
+    public class Functions
+    {
+        // public static void ProcessQueueMessage([ServiceBusTrigger("topic", "sub", Connection ="SBConnection")] string message, ExecutionContext executionContext, ILogger logger)
+        public static void ProcessMessasge([ServiceBusTrigger(topicName: "topic", subscriptionName: "sub", Connection = "SBConnection")] string myQueueItem, string MessageId, ILogger log)
+        {
+            {
+                log.LogInformation($"{myQueueItem}\n{MessageId}");
+            }
+        }
+    }
+}
